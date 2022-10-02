b34e061819979e5e676ae87ccc0f957107d93c48
Ahmed Elsadek <elecengahmed@hotmail.com>
2022-10-02 14:51:58 +0200

    adding-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Header from './components/header/Header' 
 import Nav from './components/nav/Nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Services from './components/services/services'
+import Portofolio from './components/portfolio/portfolio'
+import Testmonials from './components/testmonials/testmonials'
+import Contact from './components/contact/contact'
+import {footer as Footer} from './components/footer/footer'
+
 
 const App = () => {
   return (
     <>
     <Header />
     <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portofolio />
+    <Testmonials />
+    <Contact />
+    <Footer />
     </>
   )
 }
---
+++ src/components/testmonials/testmonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const testmonials = () => {
+  return (
+    <div>testmonials</div>
+  )
+}
+
+export default testmonials

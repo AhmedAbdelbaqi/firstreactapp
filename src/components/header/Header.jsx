f0f41046de02db934e5430e87f37fea77bf2087a
Ahmed Elsadek <elecengahmed@hotmail.com>
2022-10-03 16:56:05 +0200

    headercontentsV01

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I am </h5>
+        <h1>Ahmed Elsadek</h1>
+        <h5 className='text-light'> Senior Automation Engineer </h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
